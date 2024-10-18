449e86667d46a55a8b9fa891b492ff1150ff8ca8
parveengithub <137581429+parveengithub@users.noreply.github.com>
2024-10-18 10:07:10 -0400

    asign1

--- Assignment01/portfolio-site/app.js
+++ Assignment01/portfolio-site/app.js
@@ -1,27 +1,41 @@
-const express = require('express');
-const path = require('path');
-const exphbs = require('express-handlebars');
-const indexRouter = require('./routes/index');
+var createError = require('http-errors');
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
 
-const app = express();
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
 
-// Set up Handlebars
-app.engine('hbs', exphbs.engine({
-    extname: '.hbs',
-    defaultLayout: 'layout',
-    partialsDir: path.join(__dirname, 'views/partials') // Ensure this path is correct
-}));
+var app = express();
 
-app.set('view engine', 'hbs');
+// view engine setup
 app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'hbs');
 
-// Serve static files
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-// Use the router
 app.use('/', indexRouter);
+app.use('/users', usersRouter);
 
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
 });
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+module.exports = app;
