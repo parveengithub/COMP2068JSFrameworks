a38ad0d870ce5e241dc89b49f6230e677f454e77
parveengithub <137581429+parveengithub@users.noreply.github.com>
2024-10-18 12:32:11 -0400

    asign1

--- Assignment01/portfolio-site/routes/index.js
+++ Assignment01/portfolio-site/routes/index.js
@@ -2,8 +2,8 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/', (req, res) => res.render('home'));
-router.get('/', (req, res) => res.render('about'));
-router.get('/', (req, res) => res.render('projects'));
-router.get('/', (req, res) => res.render('contact'));
+router.get('/about', (req, res) => res.render('about'));
+router.get('/projects', (req, res) => res.render('projects'));
+router.get('/contact', (req, res) => res.render('contact'));
 
 module.exports = router;
