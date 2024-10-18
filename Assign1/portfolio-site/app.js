4ec987349415364b55705886a2b2c5672503330a
parveengithub <137581429+parveengithub@users.noreply.github.com>
2024-10-18 16:14:52 -0400

    commit

--- Assign1/portfolio-site/app.js
+++ Assign1/portfolio-site/app.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const app = express();
-const routes = require('./routes/index'); // Import the router
+const routes = require('./routes/index'); 
+const users = require('./routes/users');
 
-app.set('view engine', 'hbs'); // Set Handlebars as the view engine
+app.set('view engine'); // Set Handlebars as the view engine
+app.set('hbs');
 app.use(express.static('public')); // Serve static files
 
 app.use('/', routes); // Use the router for all routes
