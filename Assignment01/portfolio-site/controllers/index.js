5893653621bdc4cc136b4e148b375601edaf17ab
parveengithub <137581429+parveengithub@users.noreply.github.com>
2024-10-17 21:55:54 -0400

    assign

---
+++ Assignment01/portfolio-site/controllers/index.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const router = express.Router();
+
+router.get('/', (req, res) => {
+  res.render('home');
+});
+
+router.get('/about', (req, res) => {
+  res.render('about');
+});
+
+router.get('/projects', (req, res) => {
+  res.render('projects');
+});
+
+router.get('/contact', (req, res) => {
+  res.render('contact');
+});
+
+module.exports = router;
