6c445727a298a7f0ea03e856f7d16d3378e511f4
parveengithub <137581429+parveengithub@users.noreply.github.com>
2024-10-18 16:55:24 -0400

    assign1

--- Assign1/portfolio-site/app.js
+++ Assign1/portfolio-site/app.js
@@ -1,20 +1,36 @@
-var express = require('express');
-var path = require('path');
-var hbs = require('hbs');
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+const express = require('express');
+const exphbs = require('express-handlebars');
+const path = require('path');
 
-var app = express();
+const app = express();
+const PORT = process.env.PORT || 3000;
 
-// Set view engine to Handlebars
-app.set('views', path.join(__dirname, 'views'));
+// Set Handlebars as the templating engine
+app.engine('hbs', exphbs());
 app.set('view engine', 'hbs');
+app.set('views', path.join(__dirname, 'views'));
 
-// Middleware for static files
+// Serve static files from the public directory
 app.use(express.static(path.join(__dirname, 'public')));
 
-// Use the routes
-app.use('/', indexRouter);
+// Routes
+app.get('/', (req, res) => {
+  res.render('home');
+});
 
+app.get('/about', (req, res) => {
+  res.render('about');
+});
 
-module.exports = app;
+app.get('/projects', (req, res) => {
+  res.render('projects');
+});
+
+app.get('/contact', (req, res) => {
+  res.render('contact');
+});
+
+// Start the server
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
