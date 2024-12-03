be3313c02e9c4a5afc92a8635ffd0c0f52efb946
parveengithub <137581429+parveengithub@users.noreply.github.com>
2024-12-03 14:40:02 -0500

    new

--- Assignment02/local-event-planner/config/keys.js
+++ Assignment02/local-event-planner/config/keys.js
@@ -1,7 +1,7 @@
 module.exports = {
     mongoURI: 'mongodb+srv://webappadmin:<83zzzr3CsG8ipdee>@cluster0.9av9r.mongodb.net/',
-    githubClientID: 'your_github_client_id',
-    githubClientSecret: 'your_github_client_secret',
+    githubClientID: 'Ov23lihwQ54BcRrp4tJW',
+    githubClientSecret: '38247395bb7b6f30dbe59c60f4cb8a5d18e68832',
     sessionSecret: 'your_session_secret',
   };
   
