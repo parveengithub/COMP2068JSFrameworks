a277875d6dc441d744be2fed0d2652f47863cbc0
parveengithub <137581429+parveengithub@users.noreply.github.com>
2024-12-03 20:25:43 -0500

    comit

--- Assignment2/local-event-planner/app.js
+++ Assignment2/local-event-planner/app.js
@@ -3,52 +3,45 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-console.log(require('./routes/auth')); // Should output a function or router instance
-
+var app = express();
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
-var authRouter = require('./routes/auth');
-var eventRouter = require('./routes/event');
 
 var app = express();
 
-// View engine setup
+// view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
 
-// Middleware
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-// Route handlers
-app.use('/', indexRouter); // Home page
-app.use('/users', usersRouter); // Users page
-app.use('/auth', authRouter); // Authentication pages
-app.use('/events', eventRouter); // Event-related pages
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
 
-// Catch 404 and forward to error handler
-app.use(function (req, res, next) {
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
   next(createError(404));
 });
 
-// Error handler
-app.use(function (err, req, res, next) {
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // Render the error page
+  // render the error page
   res.status(err.status || 500);
   res.render('error');
 });
 
-// Start the server
-const PORT = process.env.PORT || 3000; // Ensure PORT is set for deployment environments
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+app.listen(3000, () => {
+  console.log('Server is running on port 3000');
 });
+
 
 module.exports = app;
