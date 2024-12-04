7ff0457b27f18425e31f1855b20e34885873e499
parveengithub <137581429+parveengithub@users.noreply.github.com>
2024-12-04 12:44:01 -0500

    commit

--- Assign2/LocalEventPlanner/app.js
+++ Assign2/LocalEventPlanner/app.js
@@ -27,14 +27,6 @@ app.use(function(req, res, next) {
   next(createError(404));
 });
 
-require('dotenv').config();
-
-const PORT = process.env.PORT || 4001;
-
-app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
-});
-
 // error handler
 app.use(function(err, req, res, next) {
   // set locals, only providing error in development
