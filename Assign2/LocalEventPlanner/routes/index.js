3aba4922158b57ad72f98989d7e1f8e5f0ef8217
parveengithub <137581429+parveengithub@users.noreply.github.com>
2024-12-04 14:02:13 -0500

    new

--- Assign2/LocalEventPlanner/routes/index.js
+++ Assign2/LocalEventPlanner/routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Local Event Planner' });
+  res.render('Home', { title: 'Local Event Planner' });
 });
 
 module.exports = router;
