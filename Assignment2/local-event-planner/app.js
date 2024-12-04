bd0a0be08c7fb1d4f86bc4cf29b2ed27ab1cf724
parveengithub <137581429+parveengithub@users.noreply.github.com>
2024-12-03 21:13:25 -0500

    comit

--- Assignment2/local-event-planner/app.js
+++ Assignment2/local-event-planner/app.js
@@ -39,8 +39,8 @@ app.use(function(err, req, res, next) {
   res.render('error');
 });
 
-app.listen(3000, () => {
-  console.log('Server is running on port 3000');
+app.listen(4000, () => {
+  console.log('Server is running on port 4000');
 });
 
 
