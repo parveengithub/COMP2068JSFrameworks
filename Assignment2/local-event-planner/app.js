c2fc3855a964eb52962a68bac928fe649fb23cf2
parveengithub <137581429+parveengithub@users.noreply.github.com>
2024-12-03 20:04:20 -0500

    cc

--- Assignment2/local-event-planner/app.js
+++ Assignment2/local-event-planner/app.js
@@ -3,11 +3,10 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var app = express();
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
-var authRouter = require('./routes/auth'); // Include auth routes
-var eventRouter = require('./routes/event'); // Include event routes
 
 var app = express();
 
@@ -23,16 +22,14 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
-app.use('/auth', authRouter); // Use auth routes
-app.use('/events', eventRouter); // Use event routes
 
 // catch 404 and forward to error handler
-app.use(function (req, res, next) {
+app.use(function(req, res, next) {
   next(createError(404));
 });
 
 // error handler
-app.use(function (err, req, res, next) {
+app.use(function(err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
@@ -45,5 +42,6 @@ app.use(function (err, req, res, next) {
 app.listen(3000, () => {
   console.log('Server is running on port 3000');
 });
+
 
 module.exports = app;
