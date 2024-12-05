d8438ed7c7501a1298ce12b9a109a639dc57de74
parveengithub <137581429+parveengithub@users.noreply.github.com>
2024-12-05 12:23:07 -0500

    new

--- Assign2/LocalEventPlanner/app.js
+++ Assign2/LocalEventPlanner/app.js
@@ -3,50 +3,34 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-
+var mongoose = require('mongoose');
 var indexRouter = require('./routes/index');
+var eventsRouter = require('./routes/events');
+var authRouter = require('./routes/auth');
 var usersRouter = require('./routes/users');
 
 var app = express();
+
+// Database connection
+mongoose.connect('mongodb://localhost:27017/localEventPlanner', { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => console.log('Database connected'))
+  .catch(err => console.log(err));
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
 app.use(express.static(path.join(__dirname, 'public')));
 
-// Home route
-app.get('/', (req, res) => {
-    res.render('index', { title: 'Home' });
-});
-
-// Events route
-app.get('/events', (req, res) => {
-    // You can fetch events from the database and pass them here
-    res.render('events', { title: 'Events', events: [] });
-});
-
-// Render Login Page
-app.get('/login', (req, res) => {
-  res.render('login'); // Renders the login.hbs file
-});
-
-// Render Register Page
-app.get('/register', (req, res) => {
-  res.render('register'); // Renders the register.hbs file
-});
-
-// Set up your server to listen on a port
-app.listen(3000, () => {
-    console.log('App is running on http://localhost:3000');
-});
-
+// Middleware
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
 
+// Routes
 app.use('/', indexRouter);
+app.use('/events', eventsRouter);  // Events CRUD routes
+app.use('/auth', authRouter);      // Authentication routes (login, register)
 app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
@@ -56,11 +40,8 @@ app.use(function(req, res, next) {
 
 // error handler
 app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
   res.status(err.status || 500);
   res.render('error');
 });
