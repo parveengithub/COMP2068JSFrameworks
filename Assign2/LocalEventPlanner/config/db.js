ac0adf6b005c008fd81ccb6e409dccec50fc6ed0
parveengithub <137581429+parveengithub@users.noreply.github.com>
2024-12-07 12:50:54 -0500

    comit

--- Assign2/LocalEventPlanner/config/db.js
+++ Assign2/LocalEventPlanner/config/db.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const connectDB = async () => {
   try {
-    await mongoose.connect('mongodb+srv://<local-event-planner>:<T9i9VMk0qdpoYwhS>@cluster0.9av9r.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0', { useNewUrlParser: true, useUnifiedTopology: true });
+    await mongoose.connect('s', { useNewUrlParser: true, useUnifiedTopology: true });
     console.log('MongoDB Connected');
   } catch (err) {
     console.error(err.message);
