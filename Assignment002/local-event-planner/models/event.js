8504d71452914a37a32705a1bc2d56343cf25821
parveengithub <137581429+parveengithub@users.noreply.github.com>
2024-12-07 23:28:20 -0500

    commit

--- Assignment002/local-event-planner/models/event.js
+++ Assignment002/local-event-planner/models/event.js
@@ -4,6 +4,8 @@ const eventSchema = new mongoose.Schema({
   title: { type: String, required: true },
   date: { type: Date, required: true },
   location: { type: String, required: true },
+  category: { type: String },
+  description: { type: String },
 });
 
 module.exports = mongoose.model('Event', eventSchema);
