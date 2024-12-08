09940064569d58d2ee605de7e4c8a445feaae6b6
parveengithub <137581429+parveengithub@users.noreply.github.com>
2024-12-08 09:54:43 -0500

    commit3

--- Assignment1/local-event-planner/app.js
+++ Assignment1/local-event-planner/app.js
@@ -1,41 +1,59 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+const express = require('express')
+const path = require('path')
+const app = new express()
+const hbs = require('hbs'); // Import Handlebars
+const mongoose = require('mongoose');
+const { error } = require('console');
+const User = require('./models/User.js');
+const bodyParser = require('body-parser');
+const expressSession = require('express-session');
+const flash = require('connect-flash');
+require('dotenv').config();
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+app.set('view engine', 'hbs'); // Set view engine to Handlebars
+app.set('views', path.join(__dirname, 'views')); // Set views directory
+hbs.registerPartials(path.join(__dirname, 'views/partials')); // Register partials
 
-var app = express();
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'hbs');
+app.use(express.static('public'))
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(expressSession({
+    secret: 'my secret',
+    cookie: { maxAge: 500000 },
+    saveUninitialized: false
+}));
+app.use(flash());
 
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true });
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+const homeController = require('./controllers/home');
+const loginController = require('./controllers/login');
+const signUpController = require('./controllers/signup');
+const postLoginController = require('./controllers/postLogin');
+const logoutController = require('./controllers/logout');
+const addEventController = require('./controllers/event');
+const addEventPostController = require('./controllers/eventpost');
+const updateEventController = require('./controllers/eventupdate');
+const editEventController = require('./controllers/editEvent');
+const deleteEventController = require('./controllers/deleteEvent');
+const updateEventPostController = require('./controllers/eventupdatepost');
 
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
+app.get('/', homeController);
+app.get('/login', loginController);
+app.post('/signup', signUpController);
+app.post('/postLogin', postLoginController);
+app.get('/logout', logoutController);
+app.get('/add', addEventController);
+app.post('/addEvent', addEventPostController);
+app.get('/update', updateEventController );
+app.get('/deleteEvent/:id', deleteEventController );
+app.get('/editEvent/:id', editEventController );
+app.post('/updateEvent/:id', updateEventPostController );
+
+
+
+let port = process.env.PORT || 5000; 
+app.listen(port, () => {
+    console.log('App listening on port ' + port)
 });
-
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
-});
-
-module.exports = app;
