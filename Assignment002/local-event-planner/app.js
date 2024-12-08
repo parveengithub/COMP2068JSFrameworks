e0b2c0c18f1968bcd41f70ba7454436358a6f6de
parveengithub <137581429+parveengithub@users.noreply.github.com>
2024-12-07 22:16:01 -0500

    comit

--- Assignment002/local-event-planner/app.js
+++ Assignment002/local-event-planner/app.js
@@ -4,6 +4,16 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
+
+
+require('dotenv').config();
+const connectDB = require('./config/db');
+connectDB();
+
+
+
+
+
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
